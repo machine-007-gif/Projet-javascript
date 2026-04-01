cc5d5c768699bfcea6021af4c90516e773b62b38
Marine COING-ROY <marine.coingroy@gmail.com>
2026-04-01 10:58:17 +0200

    modified:   about/about.html 	modified:   about/about.js

--- about/about.js
+++ about/about.js
@@ -1,19 +1,16 @@
-// 1. Fonction pour OUVRIR une modale spécifique
-function ouvrirModale(idDeLaModale) {
-    const modal = document.getElementById(idDeLaModale);
-    modal.style.display = "block";
+// Fonction pour ouvrir la modale ciblée
+function ouvrirModale(id) {
+    document.getElementById(id).style.display = "block";
 }
 
-// 2. Fonction pour FERMER une modale spécifique
-function fermerModale(idDeLaModale) {
-    const modal = document.getElementById(idDeLaModale);
-    modal.style.display = "none";
+// Fonction pour fermer la modale ciblée
+function fermerModale(id) {
+    document.getElementById(id).style.display = "none";
 }
 
-// 3. Fermer si on clique en dehors de la boîte blanche
+// Fermer la modale si on clique à côté de la boîte blanche
 window.onclick = function(event) {
-    // Si l'élément cliqué a la classe "modal", on le cache
-    if (event.target.classList.contains('modal')) {
+    if (event.target.className === 'modal') {
         event.target.style.display = "none";
     }
 }
